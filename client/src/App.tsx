63a4f6b50af332d41f8150ed830e4a9a41019d03
Skuming <theskumrus@gmail.com>
2025-04-30 20:50:36 +0200

    Created

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="h-12 border-2 w-12 m-4 cursor-pointer tg__btn">
+        gd
+      </button>
+    </>
+  );
+}
+
+export default App;
