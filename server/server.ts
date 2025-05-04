8fc15044ce35cedbaa7a38891bff0af120d020d9
Skuming <theskumrus@gmail.com>
2025-05-04 13:47:08 +0200

    Improved API routes

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,38 @@
-import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
+import express, { Request, Response } from "express";
+import { GetUserInfo } from "./DB/DB";
+import cors from "cors";
+import ValidateInitData from "./handlers/validate";
 
 const app = express();
 
-app.post("/api/validate", (res, req) => {});
+app.use(express.json(), cors());
 
-app.listen(process.env.PORT);
+app.post("/api/validate", async (req: Request, res: Response) => {
+  // console.log(ValidateInitData(req.body.rawdata));
 
-console.log("Server started... " + process.env.PORT);
+  if (req.body.rawdata !== "") {
+    const validateResult = await ValidateInitData(req.body.rawdata);
+    if (validateResult.status === 200) {
+      const userDBInfo = await GetUserInfo(validateResult.user.id);
+
+      const userInfo = {
+        ...userDBInfo,
+        photo_url: validateResult.user.photo_url,
+        first_name: validateResult.user.first_name,
+        lang: validateResult.user.language_code,
+      };
+      await res.status(validateResult.status).send(userInfo);
+    } else {
+      await res.status(validateResult.status).send(validateResult.error);
+    }
+  } else {
+    res.status(401).send("Use Telegram client!");
+  }
+
+  // Need to send to front-end ValidateInitData(req.body.rawdata) or maybe
+  // should add another func to validate in DB and after that send it :)
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server started... " + process.env.PORT);
+});
