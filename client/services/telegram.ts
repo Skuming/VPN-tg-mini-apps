b66b5f7cf8c2707553761408295a8f612169a308
Skuming <theskumrus@gmail.com>
2025-05-04 13:43:30 +0200

    Added API routes

--- client/services/telegram.ts
+++ client/services/telegram.ts
@@ -1,2 +1,18 @@
-/* @ts-expect-error || React can`t init this*/
-const tg = window.Telegram.WebApp;
+import axios from "axios";
+
+async function ValidateData(data: string) {
+  const response = await axios.post(
+    "https://my-mini-apps-vpn-back.loca.lt/api/validate",
+    {
+      rawdata: data,
+    },
+    {
+      headers: {
+        "bypass-tunnel-reminder": true, // remove in production
+      },
+    }
+  );
+
+  return await response.data;
+}
+export default ValidateData;
