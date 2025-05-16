878618a7037fc61df085e3bee9c58d34c309d7f3
Skuming <theskumrus@gmail.com>
2025-05-16 23:05:12 +0200

    Added types for User data context and Modal

--- client/src/interfaces/interfaces.ts
+++ client/src/interfaces/interfaces.ts
@@ -1,13 +1,27 @@
 export interface User {
-  first_name: string;
-  photo_url: string;
+  id: number;
+  user_id: number;
   balance: string;
+  have_sub: number;
+  vpn: null | string;
+  expiry_date: number | null;
+  photo_url: string;
+  first_name: string;
   lang: string;
-  vpn?: string;
-  // Can be more
+  up: number;
+  down: number;
+  expiryTime: number;
+  username: string;
 }
 
-export interface ModalFund {
-  handleClickFund: () => void;
-  lang: string | null;
+export interface InfoContextProps {
+  info: User | undefined;
+  setInfo: React.Dispatch<React.SetStateAction<User | undefined>>;
+}
+
+export interface GlobalModal {
+  heading: string;
+  content: string | undefined | null;
+  showModal: boolean;
+  setShowModal: () => void;
 }
