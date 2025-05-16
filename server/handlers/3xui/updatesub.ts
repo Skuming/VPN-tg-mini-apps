70d8c553d41b20aee42148f03803813db0b3bde0
Skuming <theskumrus@gmail.com>
2025-05-16 23:08:50 +0200

    Added renew sub funciton

---
+++ server/handlers/3xui/updatesub.ts
@@ -0,0 +1,64 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env" });
+
+async function UpdateSub(
+  userID: number,
+  username: string,
+  expirydate: number,
+  newExpiryDate: number
+) {
+  try {
+    // Игнорирование ошибок сертификатов
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+    // Авторизация
+    const authResponse = await axios.post(
+      `http://${process.env.SERVER_3X}/login`,
+      {
+        username: process.env.ADMIN_3X,
+        password: process.env.PASSWORD_3X,
+      }
+    );
+
+    const cookies = authResponse.headers["set-cookie"];
+    if (!cookies || !cookies[1]) {
+      throw new Error("Куки не найдены");
+    }
+
+    const updateClientResponse = await axios.post(
+      `http://${process.env.SERVER_3X}/panel/api/inbounds/updateClient/918919174`,
+      {
+        id: 1,
+        settings: JSON.stringify({
+          clients: [
+            {
+              id: `${userID}`,
+              flow: "",
+              email: username + expirydate,
+              limitIp: 0,
+              totalGB: "",
+              expiryTime: newExpiryDate,
+              enable: true,
+              tgId: "",
+              subId: "",
+              reset: 0,
+            },
+          ],
+        }),
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: cookies[1],
+        },
+      }
+    );
+
+    console.log(await updateClientResponse);
+  } catch (error) {
+    console.log(error);
+  }
+}
+export default UpdateSub;
