60bed4396a94a4faf9b35dca385ea572e86fa4d4
Skuming <theskumrus@gmail.com>
2025-05-16 14:49:05 +0200

    Added cookie sending method in 'api/validate' route

--- server/server.ts
+++ server/server.ts
@@ -2,35 +2,55 @@ import express, { Request, Response } from "express";
 import { GetUserInfo } from "./DB/DB";
 import cors from "cors";
 import ValidateInitData from "./handlers/validate";
+import CreateToken from "./handlers/createtoken";
+// import GetExpiryDate from "./handlers/3xui/getexpirydate";
+import GetTraficData from "./handlers/3xui/gettraficdata";
 
 const app = express();
 
-app.use(express.json(), cors());
+app.use(
+  express.json(),
+  cors({ origin: "https://my-mini-apps-vpn.loca.lt", credentials: true }) // On production change to real domain
+);
 
 app.post("/api/validate", async (req: Request, res: Response) => {
-  // console.log(ValidateInitData(req.body.rawdata));
-
   if (req.body.rawdata !== "") {
     const validateResult = await ValidateInitData(req.body.rawdata);
     if (validateResult.status === 200) {
       const userDBInfo = await GetUserInfo(validateResult.user.id);
+      const trafic = await GetTraficData(userDBInfo.user_id);
+      // const expiryTime = await GetExpiryDate(userDBInfo.user_id);
 
+      const userInfo3X = {
+        ...userDBInfo,
+        photo_url: validateResult.user.photo_url,
+        first_name: validateResult.user.first_name,
+        lang: validateResult.user.language_code.split("-")[0],
+        ...trafic,
+      };
       const userInfo = {
         ...userDBInfo,
         photo_url: validateResult.user.photo_url,
         first_name: validateResult.user.first_name,
         lang: validateResult.user.language_code.split("-")[0],
       };
-      await res.status(validateResult.status).send(userInfo);
+      const token = await CreateToken(userInfo);
+
+      await res
+        .status(validateResult.status)
+        .cookie("token", token, {
+          httpOnly: true,
+          sameSite: "none",
+          secure: true,
+          maxAge: 60 * 60 * 1000,
+        })
+        .send(userDBInfo.have_sub === 1 ? userInfo3X : userInfo);
     } else {
       await res.status(validateResult.status).send(validateResult.error);
     }
   } else {
     res.status(401).send("Use Telegram client!");
   }
-
-  // Need to send to front-end ValidateInitData(req.body.rawdata) or maybe
-  // should add another func to validate in DB and after that send it :)
 });
 
 app.listen(process.env.PORT, () => {
