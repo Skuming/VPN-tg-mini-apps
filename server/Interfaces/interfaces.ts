010a96d7bb76953e9104b4681f36d22049bdcfb8
Skuming <theskumrus@gmail.com>
2025-05-16 14:47:35 +0200

    Added interfaces for TS

---
+++ server/Interfaces/interfaces.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: number;
+  user_id: number;
+  balance: string;
+  have_sub: number;
+  vpn: null | string;
+  expiry_date: null | string | number;
+  photo_url: string;
+  first_name: string;
+  lang: string;
+}
