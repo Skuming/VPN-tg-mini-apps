5761dd54a3666bb27d64888bbe30c5070245877f
Skuming <theskumrus@gmail.com>
2025-05-16 14:45:46 +0200

    Improved DB requests

--- server/DB/DB.ts
+++ server/DB/DB.ts
@@ -1,19 +1,28 @@
 import pool from "./pool";
 
-// interface DB {
-//     userID: number;
-// }
-
 export async function GetUserInfo(userID: number) {
   const conn = await pool.getConnection();
+  await AddUserInDB(userID);
   try {
     const [result]: any = await conn.query(
       "SELECT * FROM `users` WHERE user_id = ?",
       [userID]
     );
     return result[0];
+  } finally {
+    conn.release();
+  }
+}
+
+export async function AddUserInDB(userID: number) {
+  const conn = await pool.getConnection();
+  try {
+    const [result]: any = await conn.query(
+      "INSERT INTO users (user_id) VALUES (?);",
+      [userID]
+    );
+    return result[0];
   } catch (error) {
-    console.log(error);
     conn.release();
     return null;
   } finally {
