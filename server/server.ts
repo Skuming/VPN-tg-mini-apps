339abccf1f399391501aa20a73ca097c61b8217c
Skuming <theskumrus@gmail.com>
2025-05-16 23:09:42 +0200

    Added /api/renewsub route

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,20 @@
-import express, { Request, Response } from "express";
-import { GetUserInfo } from "./DB/DB";
+import express, { Request, response, Response } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import { GetUserInfo } from "./DB/DB";
 import ValidateInitData from "./handlers/validate";
 import CreateToken from "./handlers/createtoken";
-// import GetExpiryDate from "./handlers/3xui/getexpirydate";
+import ValidateToken from "./handlers/validatetoken";
+import GetExpiryDate from "./handlers/3xui/getexpirydate";
 import GetTraficData from "./handlers/3xui/gettraficdata";
+import UpdateSub from "./handlers/3xui/updatesub";
 
 const app = express();
 
 app.use(
   express.json(),
-  cors({ origin: "https://my-mini-apps-vpn.loca.lt", credentials: true }) // On production change to real domain
+  cors({ origin: true, credentials: true }), // On production change to real domain
+  cookieParser()
 );
 
 app.post("/api/validate", async (req: Request, res: Response) => {
@@ -20,12 +24,12 @@ app.post("/api/validate", async (req: Request, res: Response) => {
       const userDBInfo = await GetUserInfo(validateResult.user.id);
       const trafic = await GetTraficData(userDBInfo.user_id);
       // const expiryTime = await GetExpiryDate(userDBInfo.user_id);
-
       const userInfo3X = {
         ...userDBInfo,
         photo_url: validateResult.user.photo_url,
         first_name: validateResult.user.first_name,
         lang: validateResult.user.language_code.split("-")[0],
+        username: validateResult.user.username,
         ...trafic,
       };
       const userInfo = {
@@ -33,6 +37,7 @@ app.post("/api/validate", async (req: Request, res: Response) => {
         photo_url: validateResult.user.photo_url,
         first_name: validateResult.user.first_name,
         lang: validateResult.user.language_code.split("-")[0],
+        username: validateResult.user.username,
       };
       const token = await CreateToken(userInfo);
 
@@ -50,6 +55,49 @@ app.post("/api/validate", async (req: Request, res: Response) => {
     }
   } else {
     res.status(401).send("Use Telegram client!");
+  }
+});
+
+app.post("/api/renewsub", async (req: Request, res: Response) => {
+  // console.log(req.cookies.token);
+  // console.log(req.body);
+
+  const days: number = req.body.renewDays;
+
+  const prices: Map<number, number> = new Map([
+    [15, 30],
+    [30, 50],
+    [45, 100],
+    [60, 120],
+    [75, 135],
+    [90, 150],
+    [105, 250],
+    [120, 300],
+  ]);
+
+  const user: any = await ValidateToken(req.cookies.token);
+
+  if (user !== "error") {
+    if (prices.has(days)) {
+      const userDBInfo = await GetUserInfo(user.user_id);
+
+      if (Number(userDBInfo.balance) >= prices.get(days)!) {
+        const expirydate = await GetExpiryDate(userDBInfo.user_id);
+        const newExpiryDate = expirydate + days * 24 * 60 * 60 * 1000;
+        await UpdateSub(
+          userDBInfo.user_id,
+          user.username,
+          expirydate,
+          newExpiryDate
+        );
+      } else {
+        res.status(404).send({ status: "Error" });
+      }
+    } else {
+      res.status(404).send({ status: "Invalid days!" });
+    }
+  } else {
+    res.status(401).send({ status: "Your token are invalid!" });
   }
 });
 
