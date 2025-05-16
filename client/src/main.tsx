498ca1f9a484ba4b273a3a1403123b031064ae4f
Skuming <theskumrus@gmail.com>
2025-05-16 23:06:48 +0200

    TG mini-apps func now init in main file and validate use context

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,38 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import { InfoContext } from "../services/context.ts";
+import ValidateData from "../services/telegram.ts";
+import { User } from "./interfaces/interfaces.ts";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+/* @ts-expect-error || React can`t init this*/
+const tg = window.Telegram.WebApp;
+const data = tg.initData;
+
+tg.expand();
+tg.enableClosingConfirmation();
+tg.setHeaderColor("#0B0C0E");
+tg.setBackgroundColor("#0B0C0E");
+
+export function Main() {
+  const [info, setInfo] = useState<User>();
+
+  useEffect(() => {
+    const validate = async () => {
+      setInfo(await ValidateData(data));
+      console.log(await ValidateData(data));
+    };
+    validate();
+  }, []);
+
+  return (
+    <StrictMode>
+      <InfoContext.Provider value={{ info, setInfo }}>
+        <App />
+      </InfoContext.Provider>
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(<Main />);
