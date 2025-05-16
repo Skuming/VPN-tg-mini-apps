6a2872b9ad473f0fc67b3ccf22f0765a7737aaa6
Skuming <theskumrus@gmail.com>
2025-05-16 23:02:29 +0200

    Updated

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,15 +5,15 @@ function Footer() {
   return (
     <footer>
       <nav className="footer__nav">
-        <a href="" className="footer__buttons">
+        <a href="#" className="footer__buttons">
           <img src={settingsImg} alt="" className="opacity-20" />
           <p className="nav__text">Настройки</p>
         </a>
-        <a href="" className="footer__buttons">
+        <a href="#" className="footer__buttons">
           <img src={homeImg} alt="" />
           <p className="nav__text">Главная</p>
         </a>
-        <a href="" className="footer__buttons">
+        <a href="#" className="footer__buttons">
           <img src={userImg} alt="" className="opacity-20" />
           <p className="nav__text">Профиль</p>
         </a>
