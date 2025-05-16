d5acfc0bd8ecb67bcfd16eee916254c49185df3d
Skuming <theskumrus@gmail.com>
2025-05-16 23:07:37 +0200

    Updated

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,40 +1,6 @@
 import HomeScreen from "../components/HomeScreen";
 import Footer from "../components/Footer";
 function Home() {
-  // /* @ts-expect-error || React can`t init this*/
-  // const tg = window.Telegram.WebApp;
-  // const data = tg.initData;
-  // tg.expand();
-  // tg.setHeaderColor("#2a2a2d");
-  // tg.setBackgroundColor("#2a2a2d");
-
-  // const [info, setInfo] = useState<User | null>(null);
-
-  // const [mobileBlur, setMobileBlur] = useState(false);
-
-  // const handleMobileblur = () => {
-  //   setMobileBlur((prev) => !prev);
-  // };
-
-  // useEffect(() => {
-  //   const validate = async () => {
-  //     const result = await ValidateData(data);
-  //     await setInfo(result);
-  //   };
-  //   validate();
-  // }, [data]);
-
-  // if (info === null)
-  //   return (
-  //     <>
-  //       <div className="flex w-full h-full justify-center items-center tg__txt flex-col">
-  //         <div className="w-[80px] h-[80px] animate-pulse bg-black rounded-full">
-  //           <div className="w-[80px] h-[80px] border-b-4 rounded-full animate-spin"></div>
-  //         </div>
-  //         {data === "" ? <p>Use telegram client!</p> : ""}
-  //       </div>
-  //     </>
-  //   );
   return (
     <>
       <HomeScreen />
