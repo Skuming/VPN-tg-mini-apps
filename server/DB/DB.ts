2378f122f533637ce60dda91c4c041b977845ab1
Skuming <theskumrus@gmail.com>
2025-05-17 22:18:56 +0200

    Added a check if user exists

--- server/DB/DB.ts
+++ server/DB/DB.ts
@@ -1,17 +1,27 @@
 import pool from "./pool";
 
 export async function GetUserInfo(userID: number) {
-  const conn = await pool.getConnection();
-  await AddUserInDB(userID);
-  try {
-    const [result]: any = await conn.query(
-      "SELECT * FROM `users` WHERE user_id = ?",
-      [userID]
-    );
-    return result[0];
-  } finally {
-    conn.release();
+  async function GetUserFromDB() {
+    const conn = await pool.getConnection();
+    try {
+      const [result]: any = await conn.query(
+        "SELECT * FROM `users` WHERE user_id = ?",
+        [userID]
+      );
+      return result[0];
+    } finally {
+      conn.release();
+    }
   }
+
+  const userInfo = await GetUserFromDB();
+
+  if (!userInfo) {
+    await AddUserInDB(userID);
+    return await GetUserFromDB();
+  }
+
+  return userInfo;
 }
 
 export async function AddUserInDB(userID: number) {
@@ -21,6 +31,24 @@ export async function AddUserInDB(userID: number) {
       "INSERT INTO users (user_id) VALUES (?);",
       [userID]
     );
+    return result[0];
+  } catch (error) {
+    conn.release();
+    return null;
+  } finally {
+    conn.release();
+  }
+}
+
+export async function ChangeBalance(userID: number, price: number) {
+  const conn = await pool.getConnection();
+  try {
+    const [result]: any = await conn.query(
+      "UPDATE `users` SET `balance` = balance - ? WHERE `user_id` = ?;",
+      [price, userID]
+    );
+    console.log(result);
+
     return result[0];
   } catch (error) {
     conn.release();
