ae5376f5b319d1c6cccdb467bb19fadd1b632b53
Skuming <theskumrus@gmail.com>
2025-05-24 21:31:38 +0200

    Added add config feature for buy vpn event

--- server/DB/DB.ts
+++ server/DB/DB.ts
@@ -1,6 +1,6 @@
 import pool from "./pool";
 
-export async function GetUserInfo(userID: number) {
+export async function GetUserInfo(userID: bigint) {
   async function GetUserFromDB() {
     const conn = await pool.getConnection();
     try {
@@ -24,7 +24,7 @@ export async function GetUserInfo(userID: number) {
   return userInfo;
 }
 
-export async function AddUserInDB(userID: number) {
+export async function AddUserInDB(userID: bigint) {
   const conn = await pool.getConnection();
   try {
     const [result]: any = await conn.query(
@@ -40,12 +40,34 @@ export async function AddUserInDB(userID: number) {
   }
 }
 
-export async function ChangeBalance(userID: number, price: number) {
+export async function ChangeBalance(userID: bigint, price: number) {
   const conn = await pool.getConnection();
   try {
     const [result]: any = await conn.query(
       "UPDATE `users` SET `balance` = balance - ? WHERE `user_id` = ?;",
       [price, userID]
+    );
+    console.log(result);
+
+    return result[0];
+  } catch (error) {
+    conn.release();
+    return null;
+  } finally {
+    conn.release();
+  }
+}
+
+export async function AddConfig(
+  userID: bigint,
+  vpn: string,
+  expiry_date: number
+) {
+  const conn = await pool.getConnection();
+  try {
+    const [result]: any = await conn.query(
+      "UPDATE `users` SET `have_sub`= 1,`vpn`= ?,`expiry_date`= ? WHERE user_id = ?",
+      [vpn, expiry_date, userID]
     );
     console.log(result);
 
