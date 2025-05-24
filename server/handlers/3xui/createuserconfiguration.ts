4eb8fc5d47007ac252c1faaf192d4467e2aaf943
Skuming <theskumrus@gmail.com>
2025-05-24 21:33:48 +0200

    Added create and delete user configuration for more efficiency

---
+++ server/handlers/3xui/createuserconfiguration.ts
@@ -0,0 +1,67 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env" });
+
+async function CreateConfig(
+  userID: bigint,
+  username: string,
+  expirydate: number
+) {
+  try {
+    // Игнорирование ошибок сертификатов
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+    // Авторизация
+    const authResponse = await axios.post(
+      `http://${process.env.SERVER_3X}/login`,
+      {
+        username: process.env.ADMIN_3X,
+        password: process.env.PASSWORD_3X,
+      }
+    );
+
+    const cookies = authResponse.headers["set-cookie"];
+    if (!cookies || !cookies[1]) {
+      throw new Error("Куки не найдены");
+    }
+
+    const createConfigResponse = await axios.post(
+      `http://${process.env.SERVER_3X}/panel/api/inbounds/addClient`,
+      {
+        id: 1,
+        settings: JSON.stringify({
+          clients: [
+            {
+              id: `${userID}`,
+              flow: "",
+              email: username + expirydate,
+              limitIp: 0,
+              totalGB: "",
+              expiryTime: expirydate,
+              enable: true,
+              tgId: "",
+              subId: "",
+              reset: 0,
+            },
+          ],
+        }),
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: cookies[1],
+        },
+      }
+    );
+    console.log(createConfigResponse.status);
+    if (createConfigResponse.status === 200) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+export default CreateConfig;
---
+++ server/handlers/3xui/deleteuserconfiguration.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../../.env" });
+
+export default async function DeleteClient(userID: bigint) {
+  try {
+    // Игнорирование ошибок сертификатов
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+    // Авторизация
+    const authResponse = await axios.post(
+      `http://${process.env.SERVER_3X}/login`,
+      {
+        username: process.env.ADMIN_3X,
+        password: process.env.PASSWORD_3X,
+      }
+    );
+
+    const cookies = authResponse.headers["set-cookie"];
+    if (!cookies || !cookies[1]) {
+      throw new Error("Куки не найдены");
+    }
+
+    const deleteClient = await axios.post(
+      `http://${process.env.SERVER_3X}/panel/api/inbounds/1/delClient/${userID}`,
+      {},
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Cookie: cookies[1],
+        },
+      }
+    );
+
+    return deleteClient;
+  } catch (error) {
+    console.log(error);
+  }
+}
